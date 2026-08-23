6d63a875abb35d4edb5d1874392b02ca36294bc8
RaafatReda1 <rafat2782005@gmail.com>
2026-08-23 20:37:30 +0300

    fundementals of Speakers Section fully done

--- src/Components/Speakers/Speakers.jsx
+++ src/Components/Speakers/Speakers.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Speakers.module.css";
 const Speakers = () => {
+  const speakers = [
+    {
+      name: "Dr. Naglaa Farouk",
+      title: "Head of Resala Medical Activity",
+      imageSrc: "./drNagalaa.png",
+    },
+    {
+      name: "Dr. Rania Abd Al-Galil",
+      title: "Head of Resala Medical Activity",
+      imageSrc: "./drRania.png",
+    },
+    {
+      name: "Dr. Rania Abd Al-Galil",
+      title: "Head of Resala Medical Activity",
+      imageSrc: "./drRania.png",
+    },
+    {
+      name: "Dr. Rania Abd Al-Galil",
+      title: "Head of Resala Medical Activity",
+      imageSrc: "./drRania.png",
+    },
+  ];
   return (
-    <div style={{ background: "red", height: "100vh", width: "100%", position: "relative" }}>Speakers</div>
-  )
-}
+    <section
+      className={styles.speakersContainer}
+      style={{
+        background: "red",
+        height: "100vh",
+        width: "100%",
+        position: "relative",
+      }}
+    >
+      {speakers.map((speaker, index) => (
+        <div key={index} className={styles.speakerCard}>
+          <img
+            src={speaker.imageSrc}
+            alt={`Speaker ${index}`}
+            className={styles.speakerImg}
+          />
+          <div className={styles.speakerInfo}>
+            <h3 className={styles.speakerName}>{speaker.name}</h3>
+            <p className={styles.speakerTitle}>{speaker.title}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default Speakers
+export default Speakers;
